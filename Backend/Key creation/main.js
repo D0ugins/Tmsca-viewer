7e106f863ef8a7589b2c7412f0c68707ed730ba2
D0ugins <yusufishabazz@gmail.com>
2020-09-26 21:12:49 -0500

    Move answer keys

--- Backend/Key creation/main.js
+++ Backend/Key creation/main.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
 
-const JSONFOLDER = 'Json Keys'
+const JSONFOLDER = '../AnswerKeys'
 
 function nsAnswer(str, num) {
     // If contains 'or' returns array of acceptable answers
     if (str.includes('or')) return str.split(' or ')
-    /* If question is multiple of 10 (estimation question) 
+    /* If question is multiple of 10 (estimation question)  
        return array where first int is min and second in is max */
-    if (num % 10 === 0) {return str.split('-').map(s => parseInt(s))}
+    if (num % 10 === 0) { return str.split('-').map(s => parseInt(s)) }
     return str
 }
 
@@ -25,9 +25,9 @@ async function parseKey(text_path) {
         // If '' at end of array remove it
         if (key[key.length - 1] === '') key.pop()
         // If key dosent have the proper number of questions print error and return
-        if (key.length !== 50 && key.length !== 80) {console.error("Incorrect number of questions for test: " + name); return;}
-        
-        
+        if (key.length !== 50 && key.length !== 80) { console.error("Incorrect number of questions for test: " + name); return; }
+
+
         let json = {
             "type": type,
             "year": path_array[2].slice(-5),
@@ -37,7 +37,7 @@ async function parseKey(text_path) {
             "penatly": type === "Number Sense" ? 4 : 2,
             "answers": {}
         }
-        
+
         /*
         Text Key formats:
         
@@ -50,10 +50,10 @@ async function parseKey(text_path) {
         */
         let count = type === "Number Sense" ? 80 : 50
         for (let i = 1; i <= count; i++) {
-                
+
             let str = key[i - 1]
-            let answer = type === "Number Sense" ? nsAnswer(str.split(") ")[1], i) 
-                        : str.split(". ")[1]
+            let answer = type === "Number Sense" ? nsAnswer(str.split(") ")[1], i)
+                : str.split(". ")[1]
 
             json.answers[i.toString()] = answer
 
@@ -63,14 +63,14 @@ async function parseKey(text_path) {
             // If parent folder dosent exist make that first
             if (!fs.existsSync(path.join(JSONFOLDER, type))) {
                 // Make base folder if it dosent exist
-                if (!fs.existsSync(JSONFOLDER)) mkdirSync(JSONFOLDER)
+                if (!fs.existsSync(JSONFOLDER)) fs.mkdirSync(JSONFOLDER)
 
                 fs.mkdirSync(path.join(JSONFOLDER, type))
             }
             fs.mkdirSync(path.join(JSONFOLDER, type, path_array[2]))
         }
         // Saves file
-        fs.writeFile(path.join(JSONFOLDER, json.path + ' Key.json'), JSON.stringify(json, null, 4), (err) => {
+        fs.writeFile(path.join(JSONFOLDER, json.path + ' Key.json'), JSON.stringify(json), (err) => {
             if (err) console.error(err)
             else console.log("Saved: " + json.path)
         })
