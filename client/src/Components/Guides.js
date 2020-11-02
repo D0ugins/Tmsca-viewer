767296fe2a630aeaa1deb12e7d26514b705e5ce4
D0ugins <yusufishabazz@gmail.com>
2020-11-01 19:51:06 -0600

    Add WIP ns explanation

--- client/src/Components/Guides.js
+++ client/src/Components/Guides.js
@@ -8,11 +8,20 @@ export default function Guides() {
             <div style={{ marginTop: "5%", textAlign: "center" }}>
                 <h1 style={{ fontSize: "3rem" }}>Math</h1>
                 <hr />
-                <div style={{ textAlign: "center", fontSize: "2rem" }}>
+                <div style={{ textAlign: "center", fontSize: "2rem", marginBottom: "5%" }}>
                     <a href="https://docs.google.com/document/d/1FCT2Q_hY0lL7t5x3yJtHRSZa9jU3llSteWEWJKlFUBQ/edit?usp=sharing"
                         style={{ marginRight: "5%" }}>
-                        Math Kickoff 20-21</a>
+                        Math Kickoff 20-21
+                    </a>
                     <a href={process.env.PUBLIC_URL + "/Explanations/Math/MSMA KO 20-21 Explanations.pdf"} download>(Download)</a>
+                </div>
+
+                <h1 style={{ fontSize: "3rem" }}>Number Sense</h1>
+                <hr />
+                <div style={{ textAlign: "center", fontSize: "2rem" }}>
+                    <a href="https://docs.google.com/document/d/18YmPXB59dQh_l5DbAvy_NQm4WUZZ-rGsgYiOfIpo_A0/edit?usp=sharing">
+                        Number Sense Kickoff 20-21 (WIP)
+                    </a>
                 </div>
 
             </div>
