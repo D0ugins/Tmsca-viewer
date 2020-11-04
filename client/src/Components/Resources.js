2246ae0d87f71cb825dcbcc1fbcf3539c79e8610
D0ugins <yusufishabazz@gmail.com>
2020-11-04 12:01:39 -0600

    Update resource link

--- client/src/Components/Resources.js
+++ client/src/Components/Resources.js
@@ -7,19 +7,19 @@ export default function Resources() {
     let base = process.env.PUBLIC_URL + "/resources/"
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <div className="resource-title">Number Sense</div>
-            <hr/>
+            <hr />
             <div className="resources-container">
                 <ul className="resource">
                     <li><a href="https://bryantheath.com/middle-school-number-sense-practice-tests/">
                         Additional Number Sense Practice Tests</a></li>
                     <li><a href={base + "BeginnerNumberSenseTricks.pdf"}>Beginner Number Sense Tricks</a></li>
-                    <li><a href="https://www.youtube.com/user/MathNinjaOrg/videos?view=0&sort=da&flow=grid">Number Sense Tricks (videos)</a></li>
                 </ul>
                 <ul className="resource">
+                    <li><a href="http://mathninja.org/math-team/number-sense-video-lesson-and-workbook/pages-4-through-24/">Number Sense Tricks + videos</a></li>
                     <li><a href={base + "NumberSesnseTricks1.pdf"}>Number Sense Tricks 1</a></li>
-                    <li><a href={base + "NumberSesnseTricks2.pdf" }>Number Sense Tricks 2</a></li>
+                    <li><a href={base + "NumberSesnseTricks2.pdf"}>Number Sense Tricks 2</a></li>
                     <li><a href={base + "NumberSesnseTricks3.pdf"}>Number Sense Tricks 3</a></li>
                 </ul>
                 <ul className="resource">
