f4044710d79a1dcf7c92034194c9dcd01cd4942f
D0ugins <yusufishabazz@gmail.com>
2020-11-14 22:34:20 -0600

    Math test 2 explanation

--- client/src/Components/Guides.js
+++ client/src/Components/Guides.js
@@ -8,12 +8,19 @@ export default function Guides() {
             <div style={{ marginTop: "5%", textAlign: "center" }}>
                 <h1 style={{ fontSize: "3rem" }}>Math</h1>
                 <hr />
-                <div style={{ textAlign: "center", fontSize: "2rem", marginBottom: "5%" }}>
+                <div style={{ textAlign: "center", fontSize: "2rem", marginBottom: "3%" }}>
                     <a href="https://docs.google.com/document/d/1FCT2Q_hY0lL7t5x3yJtHRSZa9jU3llSteWEWJKlFUBQ/edit?usp=sharing"
                         style={{ marginRight: "5%" }}>
                         Math Kickoff 20-21
                     </a>
                     <a href={process.env.PUBLIC_URL + "/Explanations/Math/MSMA KO 20-21 Explanations.pdf"} download>(Download)</a>
+                </div>
+                <div style={{ textAlign: "center", fontSize: "2rem", marginBottom: "5%" }}>
+                    <a href="https://docs.google.com/document/d/1x5Gyf7rgwUxkSj4vZvn6dGmMpfeIDJbamJq6U0DNzlo/edit?usp=sharing"
+                        style={{ marginRight: "5%" }}>
+                        Math Test 1 20-21
+                    </a>
+                    <a href={process.env.PUBLIC_URL + "/Explanations/Math/MSMA1 20-21 Explanations.pdf"} download>(Download)</a>
                 </div>
 
                 <h1 style={{ fontSize: "3rem" }}>Number Sense</h1>
